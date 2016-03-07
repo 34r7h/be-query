a08a7fb4fb9478886529cf7d4840b07c413796cc
candaras <mark.candaras@gmail.com>
2016-03-07 04:07:07 -0500

    gh-pages

---
+++ build/app/js/directives/auth-directive.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc directive
+   * @name query.directive:auth
+   * @restrict EA
+   * @element
+   *
+   * @description
+   *
+   * @example
+     <example module="query">
+       <file name="index.html">
+        <auth></auth>
+       </file>
+     </example>
+   *
+   */
+  angular.module('query').directive('auth', auth);
+
+  function auth() {
+    return {
+      restrict: 'EA',
+      templateUrl: 'directives/auth-directive.tpl.html',
+      replace: false,
+      controllerAs: 'auth',
+      controller: function controller() {
+        var vm = this;
+        vm.name = 'auth';
+      },
+      link: function link(scope, element, attrs) {
+        /* jshint unused:false */
+        /* eslint "no-unused-vars": [2, {"args": "none"}] */
+      }
+    };
+  }
+})();
+//# sourceMappingURL=auth-directive.js.map
